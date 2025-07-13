af92ecbdf27b6674782c85e8bf53d37ba9775aa3
Tagore9930 <tagore9930@gmail.com>
2025-07-13 20:27:03 +0530

    Updated files for deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const purgecss = require('gulp-purgecss')
+// const purgecss = require('gulp-purgecss') /** Specific class:  This will generate only which classes is used. */
 
 function buildStyles() {
     return src('shinobi/**/*.scss')
         .pipe(sass())
-        .pipe(purgecss({ content: ['*.html'] }))
+        // .pipe(purgecss({ content: ['*.html'] })) /** Specific class pipe code. */
         .pipe(dest('css'));
 }
 
 function watchTask() {
-    watch(['shinobi/**/*.scss', '*.html'], buildStyles);
+    watch([
+        'shinobi/**/*.scss'
+        // , '*.html' /** Specific classes checking which classes are used. */
+    ], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
